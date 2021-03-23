0ddc2f4b4f61bf67957c953b81ceade106c081c4
Lunar <nk1s.skp>
2021-03-23 10:37:35 +0100

    huge backend

--- AppProg3/AppProg3/Views/AboutPage.xaml.cs
+++ AppProg3/AppProg3/Views/AboutPage.xaml.cs
@@ -2,14 +2,33 @@
 using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Interfaces;
+using DASQL;
+using CoreAppInterface;
+using Business_Logic;
+
 
 namespace AppProg3.Views
 {
     public partial class AboutPage : ContentPage
     {
+
+        SQLController sqlc = new SQLController();
+        FrontEndInterface busic = new Business();
+
         public AboutPage()
         {
             InitializeComponent();
+            User user = Test();
+            
         }
+        
+        User Test()
+        {
+            User user = new User() { UserType = UserType.Chef, Username = "Coom", Password = "Password1", Email = "mail.fag" };
+            busic.CreateUserRandom(user);            
+            return user;
+        }
+        
     }
 }
